358551dd85c737f43b9f1c841801e359e3ab1983
davide zambon <94323992+dadezz@users.noreply.github.com>
2021-12-15 22:22:36 +0100

    Update genfromtxt.py

--- genfromtxt.py
+++ genfromtxt.py
@@ -1,22 +1,11 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import genfromtxt
+import random
 
-TOKEN="5003380254:AAGf7qVVLfB4ydCcDGvwtJOgszkXUjsJvrU"
+with open ("bastiat.txt", "r") as file_bastiat:
+    quote_bastiat = file_bastiat.read().splitlines()
+    
+def cita_bastiat ():
+    lista = len(quote_bastiat)-1
+    indice = random.randint(0, lista)
+    return quote_bastiat[indice]
 
-def bastiat(update, context):
-     frase = genfromtxt.cita_bastiat()
-     update.message.reply_text(f'{frase}')
-
-def main():
-
-   upd= Updater(TOKEN, use_context=True)
-   disp=upd.dispatcher
-
-   disp.add_handler(CommandHandler("bastiat", bastiat))
-
-   upd.start_polling()
-
-   upd.idle()
-
-if __name__=='__main__':
-   main()
+print(cita_bastiat())
