7b9739059ab6027ce606730673542349488217b0
davide zambon <94323992+dadezz@users.noreply.github.com>
2021-12-15 21:38:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import genfromtxt
+
+TOKEN="token"
+
+def bastiat(update, context):
+     frase = genfromtxt.cita_bastiat()
+     update.message.reply_text(f'{frase}')
+
+def main():
+
+   upd= Updater(TOKEN, use_context=True)
+   disp=upd.dispatcher
+
+   disp.add_handler(CommandHandler("bastiat", bastiat))
+
+   upd.start_polling()
+
+   upd.idle()
+
+if __name__=='__main__':
+   main()
