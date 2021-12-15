6b74fc92e3ae0c919bb968bef8d4240326b14c97
davide zambon <94323992+dadezz@users.noreply.github.com>
2021-12-15 22:51:54 +0100

    Update genfromtxt.py

--- genfromtxt.py
+++ genfromtxt.py
@@ -1,11 +1,15 @@
 import random
 
-with open ("bastiat.txt", "r") as file_bastiat:
-    quote_bastiat = file_bastiat.read().splitlines()
-    
 def cita_bastiat ():
-    lista = len(quote_bastiat)-1
-    indice = random.randint(0, lista)
-    return quote_bastiat[indice]
+     with open ("bastiat.txt", "r") as file_bastiat:
+          quote_bastiat = file_bastiat.read().splitlines() 
+     lista = len(quote_bastiat)-1
+     indice = random.randint(0, lista)
+     return quote_bastiat[indice]
 
-print(cita_bastiat())
+def cita_mises ():
+     with open ("mises.txt", "r") as file_mises:
+          quote_mises = file_mises.read().splitlines() 
+     lista = len(quote_mises)-1
+     indice = random.randint(0, lista)
+     return quote_mises[indice]
