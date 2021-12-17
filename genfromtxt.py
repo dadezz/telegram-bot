9a39acd33632782abd3c88b508512951c2ca384c
davide zambon <94323992+dadezz@users.noreply.github.com>
2021-12-17 03:06:51 +0100

    Update genfromtxt.py

--- genfromtxt.py
+++ genfromtxt.py
@@ -1,5 +1,7 @@
 import random
 
+citazioni_varie = {}
+
 def cita_bastiat ():
      with open ("bastiat.txt", "r") as file_bastiat:
           quote_bastiat = file_bastiat.read().splitlines() 
@@ -13,3 +15,25 @@ def cita_mises ():
      lista = len(quote_mises)-1
      indice = random.randint(0, lista)
      return quote_mises[indice]
+
+def aggiungi_cit(stringa):
+     stringa_scritta = stringa[0].split(" ", 1)
+     citazione = str(stringa_scritta[1])
+     nome = str(stringa[1])
+     if nome in citazioni_varie:
+         risposta = "occupato"
+         return risposta
+     else:
+         citazioni_varie[nome] = citazione
+         return citazioni_varie[nome]
+     
+def la_mia_cit(messaggio):
+    try:
+        provv = messaggio.split(" ", 1)
+        comando = provv[1]
+        if comando in citazioni_varie:
+            return "'"+citazioni_varie[comando]+"'"
+        else:
+            return "errore1"
+    except:
+        return "errore2"
